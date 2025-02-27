7739d27117f3a5c957ba6848f7a872ecf8c9ef3e
Jan Stenner <jan.stenner@upb.de>
2025-02-27 21:36:11 +0100

    new results

--- RBC_analyse/input_analysis.jl
+++ RBC_analyse/input_analysis.jl
@@ -6,8 +6,8 @@ first_layer_matrix = agent.policy.approximator.actor.μ.layers[1].weight
 
 
 # try state-to-y
-temp_y = reshape(env.state[:,1], 3,window_size,sensors[2])
-plot(heatmap(z=temp_y[3,:,:]'))
+# temp_y = reshape(env.state[:,1], 3,window_size,sensors[2])
+# plot(heatmap(z=temp_y[3,:,:]'))
 # passes
 
 
@@ -21,12 +21,22 @@ end
 
 
 
-temp_y = reshape(back_projection, 3,window_size,sensors[2])
+temp_y = reshape(back_projection, 3,window_size,sensors[2]+1)
 
 p = make_subplots(rows=1, cols=3)
 
-add_trace!(p, heatmap(z=temp_y[1,:,:]'), col = 1)
-add_trace!(p, heatmap(z=temp_y[2,:,:]'), col = 2)
-add_trace!(p, heatmap(z=temp_y[3,:,:]'), col = 3)
+add_trace!(p, heatmap(z=temp_y[1,:,:]', coloraxis="coloraxis"), col = 1)
+add_trace!(p, heatmap(z=temp_y[2,:,:]', coloraxis="coloraxis"), col = 2)
+add_trace!(p, heatmap(z=temp_y[3,:,:]', coloraxis="coloraxis"), col = 3)
+
+colorscale = [[0, "rgb(10, 0, 30)"], [1, "rgb(170, 100, 255)"], ]
+
+layout = Layout(
+        plot_bgcolor="#f1f3f7",
+        coloraxis = attr(cmin = 0, cmax = maximum(temp_y), colorscale = colorscale),
+    )
+
+
+relayout!(p, layout.fields)
 
 display(p)
--- RBC_analyse/window_size.jl
+++ RBC_analyse/window_size.jl
@@ -1,4 +1,5 @@
 using FileIO, JLD2
+using PlotlyJS
 
 
 
@@ -10,3 +11,81 @@ using FileIO, JLD2
 global window_size_results = FileIO.load("RBC_analyse/window_size_results.jld2","window_size_results")
 
 FileIO.save("RBC_analyse/window_size_results.jld2","window_size_results",window_size_results)
+
+
+
+
+# plot
+
+function plot_it()
+    global p
+    relayout!(p, 
+                Layout(
+                    plot_bgcolor = "white",
+                    font=attr(
+                        family="Arial",
+                        size=16,
+                        color="black"
+                    ),
+                    legend=attr(x=1.0, y=0.0,),
+                    xaxis = attr(gridcolor = "#aaaaaa",
+                                linecolor = "#888888"),
+                    yaxis = attr(gridcolor = "#aaaaaa",
+                                linecolor = "#888888"),
+                    ).fields)
+    display(p)
+end
+
+
+p = plot()
+for key in 47:-2:39
+
+    add_trace!(p, scatter(y=window_size_results[key], name="$(key)"))
+
+end
+plot_it()
+
+
+p = plot()
+for key in 37:-2:29
+
+    add_trace!(p, scatter(y=window_size_results[key], name="$(key)"))
+
+end
+plot_it()
+
+
+p = plot()
+for key in 27:-2:19
+
+    add_trace!(p, scatter(y=window_size_results[key], name="$(key)"))
+
+end
+plot_it()
+
+
+p = plot()
+for key in 17:-2:9
+
+    add_trace!(p, scatter(y=window_size_results[key], name="$(key)"))
+
+end
+plot_it()
+
+
+p = plot()
+for key in 7:-2:3
+
+    add_trace!(p, scatter(y=window_size_results[key], name="$(key)"))
+
+end
+plot_it()
+
+
+p = plot()
+
+add_trace!(p, scatter(y=window_size_results[701], name="run 1"))
+add_trace!(p, scatter(y=window_size_results[702], name="run 2"))
+add_trace!(p, scatter(y=window_size_results[703], name="run 3"))
+
+plot_it()
