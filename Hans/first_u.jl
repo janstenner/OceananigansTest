05c341369e5280010bf1c54a27af28f393db840c
Jan Stenner <jan.stenner@upb.de>
2025-03-20 16:00:55 +0100

    WIP mpc_run

--- Hans/first_u.jl
+++ Hans/first_u.jl
@@ -5,11 +5,12 @@ using Flux
 
 
 Base.@kwdef struct u_mpc
-    n_base_functions_x = 13
-    n_base_functions_t = 13
+    n_base_functions_x = 9
+    n_base_functions_t = 5
     c::Matrix{Float32} = randn(n_base_functions_x,n_base_functions_t) .* 0.2
     base_functions_space = [[x -> 1]..., [x -> sin(x*i*2π/Lx) for i in 1:Int((n_base_functions_x-1)/2)]..., [x -> cos(x*i*2π/Lx) for i in 1:Int((n_base_functions_x-1)/2)]...]
-    base_functions_time = [[t -> 1]..., [t -> sin(t*i*2π/(te/2)) for i in 1:Int((n_base_functions_t-1)/2)]..., [t -> cos(t*i*2π/(te/2)) for i in 1:Int((n_base_functions_t-1)/2)]...]
+    base_functions_time = [[t -> 1]..., [t -> sin(t*i*2π/(te/4)) for i in 1:Int((n_base_functions_t-1)/2)]..., [t -> cos(t*i*2π/(te/4)) for i in 1:Int((n_base_functions_t-1)/2)]...]
+    # base_functions_time = [t -> (t/te)^k for k in 0:Int(n_base_functions_t-1)]
 end
 
 Flux.@functor u_mpc
@@ -43,7 +44,7 @@ total_steps = 4_000
 
 u = u_mpc()
 
-new_learning_rate = 1e-1
+new_learning_rate = 1e-2
 optimizer_u = Optimisers.Adam(new_learning_rate, betas)
 u_state_tree = Flux.setup(optimizer_u, u)
 
@@ -73,7 +74,7 @@ function check_collected()
 end
 
 
-function u_mpc_train(total_steps = 100)
+function u_mpc_train(total_steps = 1_000; use_adam = true)
 
     check_collected()
 
@@ -86,26 +87,179 @@ function u_mpc_train(total_steps = 100)
     global xx = collect(Lx/actuators:Lx/actuators:Lx)
     global tt = collect(te/200:te/200:te/2)
 
+    global steps_without_improvement = 0
 
     for i in 1:total_steps
         
-        g_u = Flux.gradient(u) do u_p
+        if use_adam
+            g_u = Flux.gradient(u) do u_p
 
-            diff = u_p(xx,tt) - collected_actions
+                diff = u_p(xx,tt) - collected_actions
+                mse = mean(diff.^2)
+
+                Zygote.@ignore push!(losses, mse)
+
+                return mse
+            end
+
+            Flux.update!(u_state_tree, u, g_u[1])
+        else
+
+            old_c = copy(u.c)
+            old_diff = u(xx,tt) - collected_actions
+            old_mse = mean(old_diff.^2)
+
+            u.c[:,:] .+= 0.01 * randn(size(u.c))
+            diff = u(xx,tt) - collected_actions
             mse = mean(diff.^2)
 
-            Zygote.@ignore push!(losses, mse)
+            push!(losses, mse)
 
-            return mse
+            if mse > old_mse
+                u.c[:,:] = old_c[:,:]
+                steps_without_improvement += 1
+                println("steps without improvement: $(steps_without_improvement)")
+            else
+                steps_without_improvement = 0
+            end
         end
 
-        Flux.update!(u_state_tree, u, g_u[1])
-
         i%10 == 0 && println(i*100/total_steps, "% done")
-        println(losses[end])
     end
 
     plot(losses)
+end
+
+function get_Nusselt(temp_simulation)
+    y = zeros(3,Nx,Nz)
+
+    y[1,:,:] = temp_simulation.model.tracers.b[1:Nx,1,1:Nz]
+    y[2,:,:] = temp_simulation.model.velocities.w[1:Nx,1,1:Nz]
+
+    H = Lz
+
+    delta_T = Δb
+
+    kappa = model.closure.κ[1]
+
+    den = kappa * delta_T / H
+
+    sensordata = y[:,sensor_positions[1],sensor_positions[2]]
+
+    q_1_mean = mean(sensordata[1,:,:] .* sensordata[2,:,:])
+    Tx = mean(sensordata[1,:,:]', dims = 2)
+    q_2 = kappa * mean(array_gradient(Tx))
+
+    globalNu = (q_1_mean - q_2) / den
+
+    return globalNu
+end
+
+function evaluate_u()
+    global simulation
+    global u
+    global actions
+
+    temp_simulation = deepcopy(simulation)
+    Nusselt_agg = 0.0
+
+    for j in 1:5
+        action = u(xx, [tt[j]])
+        actions = action[:]
+        run!(temp_simulation)
+        temp_simulation.stop_time += dt
+
+        Nusselt_agg += get_Nusselt(temp_simulation)
+    end
+
+    return Nusselt_agg
+end
+
+function mpc_run()
+    global rewards = Float64[]
+    global collected_actions_2 = zeros(200,actuators)
+
+    global xx = collect(Lx/actuators:Lx/actuators:Lx)
+    global tt = collect(te/200:te/200:te/2)
+
+    global simulation
+    global model
+
+    global actions
+    
+    reward_sum = 0.0
+
+    rm("frames/", recursive=true, force=true)
+    mkdir("frames")
+
+    colorscale = [[0, "rgb(34, 74, 168)"], [0.25, "rgb(224, 224, 180)"], [0.5, "rgb(156, 33, 11)"], [1, "rgb(226, 63, 161)"], ]
+    ymax = 30
+    layout = Layout(
+            plot_bgcolor="#f1f3f7",
+            coloraxis = attr(cmin = 1, cmid = 2.5, cmax = 3, colorscale = colorscale),
+        )
+
+
+    reset!(env)
+    generate_random_init()
+
+    for i in 1:200
+
+        if i < 100
+            action = agent(env)
+        else
+            base_Nusselt = evaluate_u()
+
+            # Now that we have our base Nusselt Number, try to improvement
+            for p in 1:0
+                #TODO
+            end
+        end
+
+
+        collected_actions_2[i,:] = action[:]
+        env(action)
+
+        result = env.y[1,:,:]
+        result_W = env.y[2,:,:]
+        result_U = env.y[3,:,:]
+
+        p = make_subplots(rows=1, cols=1)
+
+        add_trace!(p, heatmap(z=result', coloraxis="coloraxis"), col = 1)
+        #add_trace!(p, heatmap(z=result_W'), col = 2)
+        #add_trace!(p, heatmap(z=result_U'), col = 3)
+
+        # p = plot(heatmap(z=result', coloraxis="coloraxis"), layout)
+
+        relayout!(p, layout.fields)
+
+        savefig(p, "frames/a$(lpad(string(i), 4, '0')).png"; width=1600, height=800)
+        #body!(w,p)
+
+        temp_reward = reward_function(env; returnGlobalNu = true)
+        println(temp_reward)
+
+        reward_sum += temp_reward
+        push!(rewards, temp_reward)
+
+        # println(mean(env.reward))
+
+        # reward_sum += mean(env.reward)
+        # push!(rewards, mean(env.reward))
+    end
+
+    println(reward_sum)
+
+
+
+    if true
+        isdir("video_output") || mkdir("video_output")
+        rm("video_output/$scriptname.mp4", force=true)
+        #run(`ffmpeg -framerate 16 -i "frames/a%04d.png" -c:v libx264 -crf 21 -an -pix_fmt yuv420p10le "video_output/$scriptname.mp4"`)
+
+        run(`ffmpeg -framerate 16 -i "frames/a%04d.png" -c:v libx264 -preset slow  -profile:v high -level:v 4.0 -pix_fmt yuv420p -crf 22 -codec:a aac "video_output/$scriptname.mp4"`)
+    end
 end
 
 
