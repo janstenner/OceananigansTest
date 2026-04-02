3850199ae45f90ec30f14f28a4887d8634bbda92
Jan Stenner <jan.stenner@upb.de>
2026-04-02 23:08:54 +0200

    run both masked und append

--- GrOWL/train_masked/masked_fixedIC.jl
+++ GrOWL/train_masked/masked_fixedIC.jl
@@ -594,7 +594,7 @@ initialize_setup()
 
 # plotrun(use_best = false, plot3D = true)
 
-function train(use_random_init = true; visuals = false, num_steps = 1600, inner_loops = 5, outer_loops = 5)
+function train(use_random_init = true; visuals = false, num_steps = 1600, inner_loops = 5, outer_loops = 10)
     
     println("MAT GO")
     frame = 1
